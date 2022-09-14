58251c7fd4ae37687ac1d75a091c9bf993963748
charlesdesenclos <75635265+charlesdesenclos@users.noreply.github.com>
2022-09-14 10:39:21 +0200

    commit 1.2

--- main.cpp
+++ main.cpp
@@ -4,17 +4,45 @@
 int main(int argc, char **argv)
 {
 
-	int plateau[64][64];
+	
+	sf::RenderWindow window(sf::VideoMode(950, 950), "Jeux d'échec");
+
+	sf::Texture image;
+
+	image.loadFromFile("./Image/echecdamier.gif");
+
+	sf::Sprite sprite;
+	image.setSmooth(true);
+
+
+	
+
+	sprite.setTexture(image);
+
+	sprite.setScale(1.6f, 1.6f);
+
+	sf::Texture image2;
+
+	image2.loadFromFile("./Image/king.png");
+
+	sf::Sprite sprite2;
+
+	sprite2.setTexture(image2);
 
 
 
 
+	
 
-	sf::Window window(sf::VideoMode(800, 600), "My window");
+
+	
+
+
 
 	// on fait tourner le programme jusqu'à ce que la fenêtre soit fermée
 	while (window.isOpen())
 	{
+		
 		// on inspecte tous les évènements de la fenêtre qui ont été émis depuis la précédente itération
 		sf::Event event;
 		while (window.pollEvent(event))
@@ -23,6 +51,11 @@ int main(int argc, char **argv)
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+		window.clear();
+		window.draw(sprite);
+		window.display();
+		
+		
 	}
 
 	return 0;
